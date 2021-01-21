e3360fb24a4b221079598989aaa852e3682cfa21
Rob Grootjen <robgrootjen1@gmail.com>
2021-01-20 18:24:12 -0600

    last update

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ let inPutField = document.getElementById('inputfield');
 
 addToDoButton.addEventListener('click',function(){
     var paragraph = document.createElement('p');
+    paragraph.classList.add('paragraph-styling')
     paragraph.innerText = inPutField.value;
     toDoContainer.appendChild(paragraph);
+    inPutField.value ="";
+    paragraph.addEventListener('click', function(){
+        paragraph.style.textDecoration = 'line-through';
+    })
+    paragraph.addEventListener('dblclick', function(){
+        toDoContainer.removeChild(paragraph);
+    })
 })
